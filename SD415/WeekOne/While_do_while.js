90f073723c77fcb8824d8423820010b119094294
Yitbarek-Wolde <yitbarek.ayni@gmail.com>
2023-10-30 21:32:12 -0500

    fixed bug

--- SD415/WeekOne/While_do_while.js
+++ SD415/WeekOne/While_do_while.js
@@ -4,7 +4,12 @@ const prompt = promptSync();
 const AGE = 18;
 
 let ask_age = Number(prompt("How old are you? "))
-while(AGE < ask_age){
-    let ask_age = Number(prompt("How old are you? "))
+while(AGE > ask_age){
+    ask_age = Number(prompt("How old are you? "))
 }
 console.log("Old enough!")
+
+do{
+   let new_ask_age = Number(prompt("How old are you? "))
+}while(new_ask_age < AGE)
+console.log("Old enough! Again!")
