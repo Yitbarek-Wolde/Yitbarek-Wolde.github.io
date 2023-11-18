77c1cbd02d131ce11df9e105382a1358553a00cf
Yitbarek-Wolde <yitbarek.ayni@gmail.com>
2023-11-17 21:43:57 -0600

    corrected dist folder

--- SD415/WeekThree/Destructure/dist/src/destructure.js
+++ SD415/WeekThree/Destructure/dist/src/destructure.js
@@ -1,52 +1,34 @@
-/* 
+/*
 Create the function topSalary(salaries) that returns the name of the top-paid person.
     If salaries is empty, it should return null.
     If there are multiple top-paid persons, return any of them.
 P.S. Use Object.entries and destructuring to iterate over key/value pairs.
 */
-
-// the following type definition says that SalaryObj has keys of type string and values of type number
-type SalaryObj = {
-    [key: string]: number;
-  };
-  
-  const salaries1: SalaryObj = {
+const salaries1 = {
     John: 100,
     Pete: 300,
     Mary: 250,
     Bob: 301,
     Alice: 400,
     Sally: 250,
-  };
-  
-  const myNull = null;
-  myNull;
-  
-  const arr = [1, 2, 3];
-  arr;
-  
-  export function topSalary(salaries: SalaryObj): string {
+};
+const myNull = null;
+myNull;
+const arr = [1, 2, 3];
+arr;
+export function topSalary(salaries) {
     if (!salaries || Object.keys(salaries).length === 0) {
-      return "none";
+        return "none";
     }
     const salariesArr = Object.entries(salaries);
-  
     let maxSalary = salariesArr[0][1]; // 100
     let maxPerson = salariesArr[0][0]; // John
-  
     salariesArr.forEach(([person, salary]) => {
-      if (salary > maxSalary) {
-        maxSalary = salary;
-        maxPerson = person;
-      }
+        if (salary > maxSalary) {
+            maxSalary = salary;
+            maxPerson = person;
+        }
     });
     return maxPerson;
-  }
-  
-  console.log(topSalary(salaries1));
-
-
-
-
-
- 
+}
+console.log(topSalary(salaries1));
